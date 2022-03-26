5a60adf1ca3bb62ff8e28df3d2476505a2e2a639
gorkavp <72318822+gorkavp@users.noreply.github.com>
2022-03-26 13:18:42 +0100

    mejora para puzle 2

--- puzle1.py
+++ puzle1.py
@@ -1,26 +1,20 @@
 import binascii
 import board
 import busio
-from digitalio import DigitalInOut
 from adafruit_pn532.i2c import PN532_I2C
 
 
 class RfidPN532:
+
+    def __init__(self):
+        self.pn532 = PN532_I2C(busio.I2C(board.SCL, board.SDA))
+        self.uid = None
+
     def read_uid(self):
-        reset_pin = DigitalInOut(borad.D6)
-        req_pin = DigitalInOut(board.D12)
-        i2c = busio.I2C(i2c, , debug=False, reset=reset_pin, req=req_pin)
-        pn532.SAM_configuration()
-        if (not pn532.firmware_version):
-            print("Error de conexión")
-        ledio = False
-        while leido == False:
-            uid = pn532.read_passive_target(timeout=0.5)
-            if uid is None:
-                continue
-            leido = True
-            uid = binsascii.hexlify(uid).decode('utf-8').upper()
-        return uid
+        self.pn532.SAM_configuration()
+        while self.uid == None:
+            self.uid = self.pn532.read_passive_target(timeout=0.5)
+        return binascii.hexlify(self.uid).decode('utf-8').upper()
 
 
 if __name__ == "__main__":
