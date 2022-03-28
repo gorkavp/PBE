f3ea4148883c1236fb794fb02494a03f2be74872
gorkavp <72318822+gorkavp@users.noreply.github.com>
2022-03-28 12:57:36 +0200

    final 2

--- puzle2.py
+++ puzle2.py
@@ -1,35 +1,29 @@
-from gi.repository import Gtk, GLib, Gdk
 from puzle1 import *
 import threading
 import gi
 
 gi.require_version("Gtk", "3.0")
-
+from gi.repository import Gtk, GLib, Gdk
 
 class Window(Gtk.Window):
 
     def __init__(self):
 
-        # Instancia de argumentos
         self.rf = RfidPN532()
 
-        # Creación de la ventana
         super().__init__(title="Puzle 2")
         self.set_position(True)
         self.set_border_width(15)
 
-        # Creación de la Box donde colocaremos los elementos de la ventana
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=15)
         self.box.set_homogeneous(False)
         self.add(self.box)
 
-        # Creación del label inicial
         self.label = Gtk.Label(label="Please, login with your university card")
         self.label.set_justify(Gtk.Justification.CENTER)
         self.label.set_size_request(500, 200)
         self.box.pack_start(self.label, True, True, 0)
 
-        # Creación y conexión del boton clear
         self.button = Gtk.Button(label="Clear")
         self.button.connect("clicked", self.on_button_clicked)
         self.box.pack_start(self.button, True, True, 0)
@@ -38,8 +32,7 @@ class Window(Gtk.Window):
         self.cssProvider = Gtk.CssProvider()
         self.cssProvider.load_from_path("style.css")
         self.styleContext = Gtk.StyleContext()
-        self.styleContext.add_provider_for_screen(Gdk.Screen.get_default(
-        ), self.cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        self.styleContext.add_provider_for_screen(Gdk.Screen.get_default(), self.cssProvider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
         self.label.get_style_context().add_class("no_uid")
         self.button.set_sensitive(False)
 
@@ -66,9 +59,6 @@ class Window(Gtk.Window):
             GLib.idle_add(self.show_uid)
         else:
             self.read_card()
-
-# Código principal programa
-
 
 def app_main():
 
